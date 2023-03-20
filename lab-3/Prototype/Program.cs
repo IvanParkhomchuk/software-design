039b018d67ad736b1343821b779dcc7f89105093
Ivan <workaccbyivan@gmail.com>
2023-03-21 01:12:02 +0200

    'mistake_fix'

--- lab-3/Prototype/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Prototype;
-
-var virusChildren = new VirusChild(new[] { new DateTime(2023, 3, 21), new DateTime(2022, 2, 22) });
-
-
-var virus = new Virus(10.12m, 2, "Some virus1", "Some type1", virusChildren);
